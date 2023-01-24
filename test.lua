75a801aade50d22672e06ec83649eec1cf1accb9
λeef <73972029+4keef@users.noreply.github.com>
2023-01-24 16:54:19 +0000

    Delete test.lua

--- test.lua
+++
@@ -1,7 +0,0 @@
-local Pastor = require './pastor.lua'
-local Books = Pastor.Books
-
-local verses = Pastor.Preach({ book = "rom",
-                              chapter = 1,
-                              ranges = { '1-2', '3-4' },
-                              translation = Books.KJV })
